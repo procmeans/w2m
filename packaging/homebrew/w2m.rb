99940ed5abeed138fa8a6e342e4ab4941d6d37ee
procmeans <procmeans@procmeanss-iMac.local>
2026-04-26 08:29:34 -0700

    ci: trim release matrix to apple-silicon-mac + x86_64-linux
    aarch64-linux cross-compile fails on chromiumoxide deps and
    x86_64-darwin (Intel Mac) runners queue 20+ minutes on free GitHub
    plan. Drop both for now; users on those platforms can install via
    'cargo install --git https://github.com/procmeans/w2m'.

--- packaging/homebrew/w2m.rb
+++ packaging/homebrew/w2m.rb
@@ -10,8 +10,7 @@ class W2m < Formula
       sha256 "REPLACE_WITH_SHA256_FROM_RELEASE"
     end
     on_intel do
-      url "https://github.com/procmeans/w2m/releases/download/v#{version}/w2m-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_FROM_RELEASE"
+      odie "Intel macOS is not currently distributed as a prebuilt binary. Build from source: cargo install --git https://github.com/procmeans/w2m"
     end
   end
 
@@ -21,8 +20,7 @@ class W2m < Formula
       sha256 "REPLACE_WITH_SHA256_FROM_RELEASE"
     end
     on_arm do
-      url "https://github.com/procmeans/w2m/releases/download/v#{version}/w2m-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_FROM_RELEASE"
+      odie "Linux ARM is not currently distributed as a prebuilt binary. Build from source: cargo install --git https://github.com/procmeans/w2m"
     end
   end
 
