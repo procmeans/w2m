9be1f112acc857441d94a24327c371a439b7c952
procmeans <procmeans@procmeanss-iMac.local>
2026-04-27 06:23:59 -0700

    build: update Formula to v0.1.2

--- packaging/homebrew/w2m.rb
+++ packaging/homebrew/w2m.rb
@@ -1,13 +1,13 @@
 class W2m < Formula
   desc "Download web pages (incl. SPAs) and convert them to Markdown"
   homepage "https://github.com/procmeans/w2m"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT" # change if you pick a different license
 
   on_macos do
     on_arm do
       url "https://github.com/procmeans/w2m/releases/download/v#{version}/w2m-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "054f230e332d70020517e01e26ff8a61da3dcd09c2e6313b337b6dfc9db6f974"
+      sha256 "7dd3ff10f285ed77249a95a24bc21ce46de9f9fe86c32854ed7053a4d3fe14e6"
     end
     on_intel do
       odie "Intel macOS is not currently distributed as a prebuilt binary. Build from source: cargo install --git https://github.com/procmeans/w2m"
@@ -17,7 +17,7 @@ class W2m < Formula
   on_linux do
     on_intel do
       url "https://github.com/procmeans/w2m/releases/download/v#{version}/w2m-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "26d8b5437db13b490225f15f7355e4a8a8f4cbac97c29bd061ebc1530ba304de"
+      sha256 "99b269c442bb442672c0e1e224672d529150effc6ef180b53876a2d084d41a11"
     end
     on_arm do
       odie "Linux ARM is not currently distributed as a prebuilt binary. Build from source: cargo install --git https://github.com/procmeans/w2m"
